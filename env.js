97ac82ec2d3c5bce5ca3e59d026f6670a63265b3
wkeybind-art <wkeybind@gmail.com>
2025-11-27 23:27:26 +0200

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,6 @@
-// env.js
+
 window.ENV = {
-  X_URL: 'https://x.com/teenwojak',          // сюда вставишь ссылку на X
-  DEXSCREENER_URL: 'https://dexscreener.com/...'    // сюда линк на embed графика
+  X_URL: 'https://x.com/teenwojak',         
+  DEXSCREENER_URL: 'https://dexscreener.com/...'    
 };
+
